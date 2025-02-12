7ecc77de7dcec0dca6f6d767f61c4812ec99c1fa
charan200415 <66833883+charan200415@users.noreply.github.com>
2025-02-12 13:07:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # Telegram API Credentials
 API_ID = "22360626"
 API_HASH = "e617b432998ddcac3f6f71b08780c926"
-BOT_TOKEN = "7395301361:AAHMvl3iskj1lt705BIFhxBY8Ohg3-5gn1k"#"7698315306:AAG9BxcSn0-JHbfwEjEFOILeOtcPmU_a6Rs"
+BOT_TOKEN = "7698315306:AAFcSnrtDEdfbS6zFC8BMRFTu_tQZqyDRWw"
 
 # FastAPI Server URL
-API_BASE_URL = "https://charan5775-testingapi.hf.space"
+API_BASE_URL = "https://zerotracex-filetolink.hf.space/"
 #API_BASE_URL = "http://127.0.0.1:7860"
 # API Docs Authentication
 DOCS_USERNAME = "admin"
